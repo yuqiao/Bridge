97b70f937691c699a536fef287fbacee16b62bc5
乔云 <rongqiao.yurq@taobao.com>
2014-07-24 13:43:10 +0800

    update.

--- go/src/hello/hello.go
+++ go/src/hello/hello.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-    who := "World!"
-    if len(os.Args) > 1 {
-        who = strings.Join(os.Args[1:], " ")
-    }
-    fmt.Println("Hello," + who)
+	who := "World!"
+	if len(os.Args) > 1 {
+		who = strings.Join(os.Args[1:], " ")
+	}
+	fmt.Println("Hello," + who)
 }
